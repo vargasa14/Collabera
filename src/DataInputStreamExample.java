794ac452d6078a93899ab96eb802616aa183b939
Alejandro <redmenace713@outlook.com>
2019-11-01 09:01:37 -0500

    Done

--- src/DataInputStreamExample.java
+++ src/DataInputStreamExample.java
@@ -8,10 +8,11 @@ public class DataInputStreamExample {
 	public static void main(String[] args)  throws IOException {
 		
 		File file = new File ("data.dat");
+		
 		FileInputStream fis = new FileInputStream(file);
 		DataInputStream dis = new DataInputStream(fis);
 		
-		int data = dis.read();
+		int data = dis.readInt();
 		System.out.println("Data Read: " + data);
 		System.out.println("Data Read: " + dis.readBoolean());
 		System.out.println("Data Read: " + dis.readChar() );
--- src/DataOutputStreamExample.java
+++ src/DataOutputStreamExample.java
@@ -9,17 +9,16 @@ public class DataOutputStreamExample {
 	public static void main(String[] args) {
 		File file = new File ("data.dat");
 		try {
-				FileOutputStream fos = new FileOutputStream(file);
-				DataOutputStream dos = new DataOutputStream(fos);
-				dos.writeInt(12345);
-				dos.writeBoolean(true);
-				dos.writeChar('W');
-				dos.close();	
+			FileOutputStream fos = new FileOutputStream(file);
+			DataOutputStream dos = new DataOutputStream(fos);
+			dos.writeInt(12345);
+			dos.writeBoolean(true);
+			dos.writeChar('W');
+			dos.close();	
 		} catch (FileNotFoundException x) {
 			System.out.println("FileNotFoundException: " + x.getMessage());
 		}
-		catch (IOException e) 
-		{
+		catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
